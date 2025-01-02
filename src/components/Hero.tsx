c6728af1cdf2d482972049d66dc68c2cfc9433b7
Shashank <113260615+shashankaz@users.noreply.github.com>
2025-01-02 10:38:21 +0530

    update Hero component layout for improved spacing

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 const Hero = () => {
   return (
-    <main className="flex flex-col-reverse md:flex-row items-start md:items-center justify-between gap-10 py-8">
+    <main className="flex flex-col-reverse md:flex-row items-start md:items-center justify-between gap-4 md:gap-10 py-8">
       <div>
         <h1 className="text-3xl font-bold flex flex-col md:flex-row md:items-end gap-2">
           Hey, I&apos;m Shashank
